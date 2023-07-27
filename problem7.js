ee39ccd3293ceb886ff1e99b1dac9186b227e241
Anik Islam <aanikbsmrstucse@gmail.com>
2023-07-27 18:25:41 +0600

    problem seven  solve

---
+++ problem7.js
@@ -0,0 +1,31 @@
+function romanToInt(romanNumeral) {
+    const romanValues = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+    };
+  
+    let result = 0;
+  
+    for (let i = 0; i < romanNumeral.length; i++) {
+      const currentChar = romanNumeral[i];
+      const nextChar = romanNumeral[i + 1];
+  
+      if (romanValues[currentChar] < romanValues[nextChar]) {
+        result -= romanValues[currentChar];
+      } else {
+        result += romanValues[currentChar];
+      }
+    }
+  
+    return result;
+  }
+  
+  
+  console.log(romanToInt("IX")); 
+  console.log(romanToInt("XXI"));
+  
